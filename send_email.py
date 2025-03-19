3f9c0f99bcf820a3ccb87b218e5bfdc252114e07
karanaggarwal1583 <karanagg1001@gmail.com>
2025-03-19 14:14:02 +0530

    Second Commit

--- send_email.py
+++ send_email.py
@@ -20,17 +20,48 @@
 # print("Email sent successfully!")
 
 
+# import smtplib
+# from email.message import EmailMessage
+
+# SMTP_SERVER = "smtp.gmail.com"
+# SMTP_PORT = 587
+# EMAIL_ADDRESS = "karanagg1001@gmail.com"
+# EMAIL_PASSWORD = "tjlldxzkqfxybtgz"
+
+# def send_email(subject, body):
+#     to_emails = ["karanagg2002@gmail.com"]
+#     cc_emails = ["kanikaagg2001@gmail.com","rushilagarwal08@gmail.com"]
+
+#     msg = EmailMessage()
+#     msg["From"] = EMAIL_ADDRESS
+#     msg["To"] = ", ".join(to_emails)
+#     msg["Cc"] = ", ".join(cc_emails)
+#     msg["Subject"] = subject
+#     msg.set_content(body)
+
+#     with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
+#         server.starttls()
+#         server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+#         all_recipients = to_emails + cc_emails
+#         server.send_message(msg, to_addrs=all_recipients)
+
+#     print("Email sent successfully!")
+
+
+
+
 import smtplib
+import os
 from email.message import EmailMessage
 
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
-EMAIL_ADDRESS = "karanagg1001@gmail.com"
-EMAIL_PASSWORD = "tjlldxzkqfxybtgz"
+EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")  # Fetch from environment
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")  # Fetch from environment
 
 def send_email(subject, body):
     to_emails = ["karanagg2002@gmail.com"]
-    cc_emails = ["kanikaagg2001@gmail.com","rushilagarwal08@gmail.com"]
+    cc_emails = ["kanikaagg2001@gmail.com", "rushilagarwal08@gmail.com"]
 
     msg = EmailMessage()
     msg["From"] = EMAIL_ADDRESS
@@ -46,7 +77,3 @@ def send_email(subject, body):
         server.send_message(msg, to_addrs=all_recipients)
 
     print("Email sent successfully!")
-
-
-
-
